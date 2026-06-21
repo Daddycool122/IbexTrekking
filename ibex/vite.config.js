4a5e9b5500a67f23e3f6508ff9e861317503f01a
Daddycool122 <akhileshramola.2000@gmail.com>
2026-06-21 23:11:54 +0530

    Updated  SEWO 2

--- ibex/vite.config.js
+++ ibex/vite.config.js
@@ -1,23 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import vitePrerender from "vite-plugin-prerender-esm-fix";
-import path from "path";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    vitePrerender({
-      staticDir: path.join(__dirname, 'dist'),
-      routes: [
-        '/',
-        '/Trekking',
-        '/valley-of-flowers-trek',
-        '/chopta-chandrashilla-trek',
-        '/Rafting',
-        '/Tours'
-      ]
-    })
-  ],
+  plugins: [react()],
   server: {
     allowedHosts: ["ibexiceaxe.com", "www.ibexiceaxe.com"], // ✅ Add both
     host: true,
